65b0cdba6be1cc120fe8ca3cb94a34e824d20199
RSBZeph <RSBZeph@gmail.com>
2023-02-12 16:29:01 +0100

    read photos from folder

--- CV_1.py
+++ CV_1.py
@@ -2,10 +2,10 @@ import numpy as np
 import cv2 as cv
 import glob
 
-x = 8
-y = 11
+x = 6
+y = 9
 # termination criteria
-print("it works")
+
 #test
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
@@ -14,7 +14,8 @@ objp[:,:2] = np.mgrid[0:y,0:x].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
-images = glob.glob('*.jpg')
+images = glob.glob('C:\\Users\\rsbze\\Desktop\\Repos\\Uni\\Computer_vision\\test_images\\*.jpg')
+print("it works")
 for fname in images:
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -22,11 +23,21 @@ for fname in images:
     ret, corners = cv.findChessboardCorners(gray, (y,x), None)
     # If found, add object points, image points (after refining them)
     if ret == True:
+        print("found")
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
         # Draw and display the corners
+        cv.namedWindow("resize", cv.WINDOW_NORMAL)
+        cv.resizeWindow("resize", 1000, 1000)
         cv.drawChessboardCorners(img, (y,x), corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(500)
+        cv.imshow('resize', img)
+        cv.waitKey(2500)
+
 cv.destroyAllWindows()
+
+
+#    cv.namedWindow("ja", cv.WINDOW_NORMAL)
+#    cv.resizeWindow("ja", 500, 500)
+#    cv.imshow('ja', img)
+#    cv.waitKey(500)
