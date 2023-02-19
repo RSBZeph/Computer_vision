a3d0524e5f7e2a2d3b7a62d83935bb64f49e1c49
LAPTOP-7C6CM570\yoran <yorandenheijer@gmail.com>
2023-02-19 13:33:05 +0100

    Final changes for test runs

--- CV_1.py
+++ CV_1.py
@@ -11,7 +11,8 @@ import glob
 
 x = 6
 y = 9
-win_size = 800
+win_sizeX = 1280
+win_sizeY = 720 
 main_corners = []
 live = False
 
@@ -90,25 +91,24 @@ objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
 #read photos from folder
-images = glob.glob('C:\\Users\\rsbze\\Desktop\\Repos\\Uni\\Computer_vision\\short_test\\*.jpg')
-drawimages = glob.glob('C:\\Users\\rsbze\\Desktop\\Repos\\Uni\\Computer_vision\\draw\\*.jpg')
-interpolationimages = glob.glob('C:\\Users\\rsbze\\Desktop\\Repos\\Uni\\Computer_vision\\test_interpolation\\*.jpg')
-#images = glob.glob('C:\\Users\\yoran\\Documents\\UU\\GMT\\Jaar1\\P3\\Computer_vision\\ComputerVisionP1\\draw\\*.jpg')
-#drawimage = glob.glob('C:\\Users\\yoran\\Documents\\UU\\GMT\\Jaar1\\P3\\Computer_vision\\ComputerVisionP1\\draw\\*.jpg')
+#images = glob.glob('C:\\Users\\rsbze\\Desktop\\Repos\\Uni\\Computer_vision\\short_test\\*.jpg')
+#drawimages = glob.glob('C:\\Users\\rsbze\\Desktop\\Repos\\Uni\\Computer_vision\\draw\\*.jpg')
+#interpolationimages = glob.glob('C:\\Users\\rsbze\\Desktop\\Repos\\Uni\\Computer_vision\\test_interpolation\\*.jpg')
+images = glob.glob('C:\\Users\\yoran\\Documents\\UU\\GMT\\Jaar1\\P3\\Computer_vision\\ComputerVisionP1\\run1\\*.jpg')
+drawimages = glob.glob('C:\\Users\\yoran\\Documents\\UU\\GMT\\Jaar1\\P3\\Computer_vision\\ComputerVisionP1\\test\\*.jpg')
 
 counter = 0
-for fname in drawimages:
+for fname in images:
     counter += 1
     img = cv.imread(fname)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # Find the chess board corners
     ret, corners = cv.findChessboardCorners(gray, (y,x), cv.CALIB_CB_FAST_CHECK)
-    ret = True
     print(str(counter) + " " + str(ret))
     # make new resized window
     cv.namedWindow("resize", cv.WINDOW_NORMAL)
-    cv.resizeWindow("resize", win_size, win_size)
+    cv.resizeWindow("resize", win_sizeX, win_sizeY)
     # if the corners are not automatically found then manually select corners
     if ret == False:
         cv.imshow('resize', img)
@@ -118,6 +118,7 @@ for fname in drawimages:
         corners = make_grid()
         main_corners.clear()
         corners = np.float32(corners)
+        ret = True
 
     # add objects to list and apply function to make the corners more accurate
     objpoints.append(objp)
@@ -129,34 +130,29 @@ for fname in drawimages:
     cv.imshow('resize', img)
 
     cv.waitKey(1000)
+    
 
 #calibration
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-np.savez("CameraParams", mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+#np.savez("CameraParams", mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+print(str(mtx))
 # undistortion
-img = cv.imread('test1_first.jpg')
-h,  w = img.shape[:2]
-newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+#img = cv.imread('test1_first.jpg')
+#h,  w = img.shape[:2]
+#newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
 # print the camera intrinsics
-print(str(newcameramtx))
+#print(str(newcameramtx))
 
-dst = cv.undistort(img, mtx, dist, None, newcameramtx)
-
-mean_error = 0
-for i in range(len(objpoints)):
-    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
-    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
-    mean_error += error
-print( "total error: {}".format(mean_error/len(objpoints)) )
+#dst = cv.undistort(img, mtx, dist, None, newcameramtx)
 
 cv.destroyAllWindows()
 
 
 # online fase
 # Load previously saved data
-with np.load('CameraParams.npz') as X:
-    mtx, dist, _, _ = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
+#with np.load('CameraParams.npz') as X:
+#    mtx, dist, rvecs, tvecs = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
 
 #How axis look like
 axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
@@ -168,7 +164,7 @@ if live == False:
     for fname in drawimages:
         counter += 1
         cv.namedWindow("img", cv.WINDOW_NORMAL)
-        cv.resizeWindow("img", win_size, win_size)
+        cv.resizeWindow("img", win_sizeX, win_sizeY)
         img = cv.imread(fname)
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         # Find the chess board corners
@@ -183,15 +179,15 @@ if live == False:
             imgpts, jac2 = cv.projectPoints(cube, rvecs, tvecs, mtx, dist)
             imgpts2, jac = cv.projectPoints(axis, rvecs, tvecs, mtx, dist)
             # draw axis and cube on the picture
-            img = draw_cube(img,corners2,imgpts)
             img = draw_axis(img,corners2,imgpts2)
-
+            img = draw_cube(img,corners2,imgpts)
             cv.imshow('img',img)
-            cv.waitKey(2000)
+            cv.waitKey(0)
 
 #webcam
 if live == True:
-    cap = cv.VideoCapture(0, cv.CAP_DSHOW) # this is the magic!
+    cap = cv.VideoCapture(0, cv.CAP_DSHOW)
+    #cap.set(cv.CAP_PROP_BUFFERSIZE, 3)
     cap.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
 
